aa5947dd31a1a6fd15a30e877885bb46b9c1b7c3
bchigoy1 <bchigoy@gmail.com>
2025-01-07 20:56:03 -0500

    Initial commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,13 +34,32 @@ function Projects({ projects, sectionId = 'projects' }) {
       <div className="flex justify-between items-center">
         <button
           onClick={toggleContent}
-          className={`mt-4 p-2 rounded text-white font-bold text-lg ${
-            isOpen
-              ? 'bg-gray-500 text-black hover:bg-gray-500 text-left'
-              : 'bg-gray-500 text-black hover:bg-gray-500 text-left'
-          } w-full`}
+          className={`
+            mt-4 
+            p-3 
+            rounded 
+            font-bold 
+            text-lg 
+            w-full 
+            text-left
+            touch-manipulation
+            active:bg-gray-600
+            ${isOpen ? 'bg-gray-500 text-white' : 'bg-gray-500 text-white'}
+            flex 
+            items-center 
+            justify-between
+          `}
         >
-          {isOpen ? 'Hide Projects' : 'Projects'}
+          <span>{isOpen ? 'Hide Projects' : 'Projects'}</span>
+          <svg 
+            className={`w-6 h-6 transform transition-transform ${isOpen ? 'rotate-180' : ''}`} 
+            fill="none" 
+            stroke="currentColor" 
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+          </svg>
         </button>
       </div>
 
